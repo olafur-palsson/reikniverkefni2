565de2d61899bef5358cf9a13c9b602df756c8dc
olafur-palsson <olafur.palsson2@gmail.com>
2018-10-21 13:47:53 +0000

    Update

--- Backgammon2/neuralNetwork.py
+++ Backgammon2/neuralNetwork.py
@@ -8,14 +8,14 @@ from functools import reduce
 from torch.autograd import Variable
 
 
-learning_rate = 1e-3
+learning_rate = 1e-2
 dtype = torch.float
 device = torch.device("cpu")
 device = torch.device("cuda:0") # Uncomment this to run on GPU
 
 input_width, output_width = 102, 1
 # hidden_layers_width = [500, 100, 100, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 55, output_width]
-hidden_layers_width = [50, 50, 50, 50, 50]
+hidden_layers_width = [200, 200, 200, 200, 200, 200, 200]
 
 
 def make_layers():
