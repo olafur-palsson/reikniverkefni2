cb93fa9ccb65b967306710883890725eb16abbfb
olafur-palsson <olafur.palsson2@gmail.com>
2018-10-21 19:28:17 +0000

    Long test

--- Backgammon2/neuralNetwork.py
+++ Backgammon2/neuralNetwork.py
@@ -15,7 +15,7 @@ device = torch.device("cuda:0") # Uncomment this to run on GPU
 
 input_width, output_width = 102, 1
 # hidden_layers_width = [500, 100, 100, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 55, output_width]
-hidden_layers_width = [300, 250, 200, 200, 200]
+hidden_layers_width = [200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200]
 
 
 def make_layers():
